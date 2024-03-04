7a0d5a082631fabc8ea0f2c3082130029e79bfa4
marcin.gawron <marcin.gawron@S50-04>
2024-03-04 10:28:10 +0100

    zegar skonczony

--- zegarnik/zegarnik/App.xaml.cs
+++ zegarnik/zegarnik/App.xaml.cs
@@ -10,7 +10,7 @@ namespace zegarnik
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new mainTabbed());
         }
 
         protected override void OnStart()
---
+++ zegarnik/zegarnik/mainTabbed.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zegarnik
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class mainTabbed : TabbedPage
+    {
+        public mainTabbed()
+        {
+            InitializeComponent();
+            UpdateDateTime(null, null);
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += UpdateDateTime;
+            timer.Start();
+        }
+        public void UpdateDateTime(object sunder, ElapsedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+                dateLabel.Text = DateTime.Now.ToString("dd MMMM yyyy");
+            });
+        }
+    }
+}
